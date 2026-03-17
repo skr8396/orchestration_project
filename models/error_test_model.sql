ebd55b9c4b0ca1c2ebd8ef4872d68f883b7398e8
skr8396 <kk3204030@gmail.com>
2026-03-17 12:12:48 +0530

    Update error_test_model.sql

--- models/error_test_model.sql
+++ models/error_test_model.sql
@@ -4,7 +4,6 @@
 
 -- This will FAIL with invalid column error at execution
 SELECT 
-  cs_customer_id,  -- This column doesn't exist
-  c_customer_sk
+ *
 FROM SNOWFLAKE_SAMPLE_DATA.TPCDS_SF100TCL.CUSTOMER
 LIMIT 10
